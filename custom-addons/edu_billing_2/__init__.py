b3b12617ef689a12f976690238c0efc6e5ff0f37
islom2020 <islomjonanvarjonov@gmail.com>
2025-10-24 22:03:42 +0500

    fldmg;ldf

---
+++ custom-addons/edu_billing_2/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+# Init for edu_billing
---
+++ custom-addons/edu_billing_2/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Edu Billing",
+    "summary": "Minimal sample module to test Import Module in Odoo 19",
+    "version": "19.0.1.0.0",
+    "category": "Tools",
+    "author": "Your Company",
+    "website": "https://example.com",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [
+        "views/edu_billing_menus.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
